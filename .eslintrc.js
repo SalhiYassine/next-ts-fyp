eedf3ce755992e6c106d2499893e7882819a5215
Yassine Salhi <yassinesalhi135@gmail.com>
2021-11-09 15:57:26 +0000

    final setup, eslint, ready to go

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,10 +23,18 @@ module.exports = {
   rules: {
     "require-jsdoc": 0,
     "react/react-in-jsx-scope": 0,
-    "no-console": 1,
+    "no-console": 2,
     "react/jsx-uses-vars": 2,
     "no-undef": 0,
-    "": 0,
+    "no-unreachable": 2,
+    "default-case": 2,
+    "default-case-last": 2,
+    eqeqeq: 2,
+    "no-empty": 2,
+    "no-empty-function": 2,
+    "require-await": 2,
+    "dot-location": 2,
+    "no-unused-vars": 2,
   },
   settings: {
     react: {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from '../redux/store'
-import getAuthStatus from "../redux/auth/Actions/AuthenticationStatus/AuthenticationStatusAction";
 
 export default function Home() {
-  const state = useSelector((state: RootState) => state)
+  // const state = useSelector((state: RootState) => state)
 
   return (
     <>
